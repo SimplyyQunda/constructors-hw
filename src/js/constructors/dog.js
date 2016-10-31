5e63e674060135489627e1be887799394c92e7d0
DaQunda Magwood <dmagwood13@gmail.com>
2016-10-30 22:35:01 -0400

    Everything has finally been passed

--- src/js/constructors/dog.js
+++ src/js/constructors/dog.js
@@ -1,4 +1,16 @@
-function Dog () {
+function Dog (choices) {
+
+	var choice = choices || {};	
+	this.status = "normal";
+	this.color = "black";
+	
+	
+if (choice.hungry === undefined) {
+	this.hungry = true;
+} else {
+	this.hungry = choice.hungry;
+	}
+	
 }
 
 export { Dog };
--- src/js/constructors/person.js
+++ src/js/constructors/person.js
@@ -1,4 +1,24 @@
-function Person () {
+function Person (choices) {
+	
+	this.pet = function(dog){
+		dog.status = 'happy';
+	};
+	this.feed = function(dog){
+		dog.hungry = false;
+	};
+
+	var choice = choices || {};	
+
+if (choice.cool === undefined) {
+	this.cool= false;
+} else {
+	this.cool = choice.cool;
+	}
+	
 };
+	
+
+
+
 
 export { Person };
